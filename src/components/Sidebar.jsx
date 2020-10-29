5bde1ab8159f1c249622ba3adf0c34611c6a2c15
Autumn Pham <autumnpham@Autumns-MacBook.lan>
2020-10-28 20:03:44 -0700

    margins

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,9 +8,11 @@ class Sidebar extends React.Component {
           <img src={Autumn} alt="autumn headshot" className="autumn-img"/>
           <h3 className="name">Autumn Pham</h3>
           <span className="email desktop-display"><i className="far fa-envelope"></i> ph.autumn@gmail.com</span><hr/>
-          <a href="#about">About</a><br/>
-          <a href="#projects">Projects</a><br/>
-          <a href="#experience">Experience</a><hr/>
+          <div className="page-links">
+            <a href="#about">About</a><br/>
+            <a href="#projects">Projects</a><br/>
+            <a href="#experience">Experience</a>
+          </div><hr/>
           <div className="social-icons">
             <a href="https://www.linkedin.com/in/autumn-pham" target="_blank" rel="noreferrer"><i className="fab fa-linkedin fa-2x"></i></a><br/>
             <a href="https://github.com/autumn-pham" target="_blank" rel="noreferrer"><i className="fab fa-github-square fa-2x"></i></a><br/>
