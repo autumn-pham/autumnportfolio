b19d2bfb13bb30b590a6df7cd4c18161532804cb
Autumn Pham <autumnpham@Autumns-MacBook.lan>
2020-10-30 19:54:39 -0700

    added resume link to main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -19,7 +19,7 @@ class Main extends React.Component {
             <div className="experience-text"><span className="title">General Assembly</span><span className="date">Aug 2020-Nov 2020</span><br/>13-week, full-time immersive course on web development. Curriculum included HTML, CSS, JavaScript, PHP, modern front-end frameworks, back-end web app and API development with modern programming languages and MVC, and consummation of third-party APIs. Emphasized collaboration in teams using agile development workflow and widely accepted collaboration practices in Git/Github.</div>
           </div>
 
-        <p>View my full resume <a href="#">here</a>.</p>
+        <p>View my full resume <a href="https://drive.google.com/file/d/1Z96H84gf3LnSVJ_u3NGJR7h1FKdcNFnz/view?usp=sharing" target="_blank" rel="noreferrer">here</a>.</p>
       </div>
     );
   }
